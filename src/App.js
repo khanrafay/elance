8a440028717f98722c963f8477ea3f2f9a20ca89
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2021-04-07 14:19:11 +0500

    worked on login and signup page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './containers/Main/main';
+import Login from './containers/Login/login';
+import Signup from './containers/Signup/Signup';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/">
+          <Main />
+        </Route>
+      </Switch>
+    </Router>
+
   );
 }
 
